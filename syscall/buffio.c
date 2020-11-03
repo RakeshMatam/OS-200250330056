31c4acbbfae0a8d07bea096bb48996839b7f5dfe
Rakesh Matam <rakeshmattam1000@gmail.com>
2020-11-03 05:42:53 +0530

    Demo for Buffio

---
+++ syscall/buffio.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello\n");
+    printf("World\n");
+    return 0;
+}
+
+// when no "\n" given the system called write function only once as per the above code
+// When "\n" given for two printf statements they the write function is called two times 
+// use "strace ./buffio " - to check the responce
